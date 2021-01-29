b244f0924002a532048aa7d9562a25b4d84b67d8
Sam Rodgers <srodgersns@gmail.com>
2021-01-29 15:18:08 -0600

    more notes, plus update link to stylesheet

--- reference/header.php
+++ reference/header.php
@@ -14,7 +14,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="style/web_style.css">
   <? if (isset($otherMetaData)) echo $otherMetaData;?>
 </head>
 <body>
@@ -49,6 +49,7 @@
     </div>
     <div class="col-4">
       <div class="profile-options-header">
+         <-- I'm also gonna need the same drop-down function for the login info -->
         <p><a href="../log-in.php">Log In</a> | <a href="../sign-up.php">Sign Up</a></p>
       </div>
     </div><? } ?>
